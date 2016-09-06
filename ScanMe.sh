96487058237d79d17d32aaa7e2455958dcb0cd34
Hal Hearst <hhearst@bds-ad.lc>
2016-09-06 15:25:46 -0400

    adding changes

---
+++ ScanMe.sh
@@ -0,0 +1,1 @@
+sh ./scan.cli-3.3.0/bin/scan.cli.sh --username CircleCI --password blackduck --scheme https --host saleshub.blackducksoftware.com --port 443 ./ --project "WebGoat Parent Pom" --release 7.1-SNAPSHOT --verbose
