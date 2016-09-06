05a13c51b52ad37ef8a043f87b5380206c4f7234
Hal Hearst <hhearst@bds-ad.lc>
2016-09-06 17:54:57 -0400

    test

--- ScanMe.sh
+++ ScanMe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 java -version
-java -Done-jar.silent=true -Done-jar.jar.path=/home/ubuntu/WebGoat/scan.cli-3.3.0/lib/cache/scan.cli.impl-standalone.jar -Xmx4096m -jar /home/ubuntu/WebGoat/scan.cli-3.3.0/lib/scan.cli-3.3.0-standalone.jar --scheme https --host saleshub.blackducksoftware.com --username CircleCI --password blackduck --port 443 --verbose --project "WebGoat Parent Pom" --release 7.1-SNAPSHOT /home/ubuntu/WebGoat
+java -Done-jar.silent=true -Done-jar.jar.path=/home/ubuntu/WebGoat/scan.cli-3.3.0/lib/cache/scan.cli.impl-standalone.jar -Xmx4096m -jar /home/ubuntu/WebGoat/scan.cli-3.3.0/lib/scan.cli-3.3.0-standalone.jar --scheme https --host saleshub.blackducksoftware.com --username CircleCI --password blackduck --port 443 --verbose --project "WebGoat Parent Pom" --release 7.1-SNAPSHOT /home/ubuntu/WebGoat/webgoat-container/target/
 #pwd
 #chmod -R 777 /home/ubuntu/WebGoat/scan.cli-3.3.0
 #export BDS_JAVA_HOME=/home/ubuntu/WebGoat/scan.cli-3.3.0/jre
