edec40c998cf9b00906bc64e4c4768275d1a2c1b
Hal Hearst <hhearst@bds-ad.lc>
2016-09-06 16:35:41 -0400

    test

--- ScanMe.sh
+++ ScanMe.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pwd
-export BDS_JAVA_HOME=/home/ubuntu/WebGoat/scan.cli-3.3.0/jre
+export BDS_JAVA_HOME=/home/ubuntu/WebGoat/scan.cli-3.3.0/jre/bin
 echo $BDS_JAVA_HOME
 ls $BDS_JAVA_HOME
 sh ./scan.cli-3.3.0/bin/scan.cli.sh --username CircleCI --password blackduck --scheme https --host saleshub.blackducksoftware.com --port 443 ./ --project "WebGoat Parent Pom" --release 7.1-SNAPSHOT --verbose
