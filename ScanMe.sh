f2bd84f2468f6711e95f9c8317ada19892773d5c
Hal Hearst <hhearst@bds-ad.lc>
2016-09-06 16:17:00 -0400

    test

--- ScanMe.sh
+++ ScanMe.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+pwd
 sh ./scan.cli-3.3.0/bin/scan.cli.sh --username CircleCI --password blackduck --scheme https --host saleshub.blackducksoftware.com --port 443 ./ --project "WebGoat Parent Pom" --release 7.1-SNAPSHOT --verbose
